4bd2bee00723aa076dd0fc07f08d9f3abe92465a
Ludacris-Plaid <indicationsmedia@protonmail.com>
2022-01-01 23:10:38 -0700

    test

---
+++ build/precache-manifest.aa63b6ea8153adcbdca5bc62e5418e48.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c874c2b70ed53f2cbb43",
+    "url": "/Our_Social_Media/static/css/main.b6c23002.chunk.css"
+  },
+  {
+    "revision": "c874c2b70ed53f2cbb43",
+    "url": "/Our_Social_Media/static/js/main.c874c2b7.chunk.js"
+  },
+  {
+    "revision": "f0e0f9773d07a5a0df2a",
+    "url": "/Our_Social_Media/static/css/1.1e4ccc00.chunk.css"
+  },
+  {
+    "revision": "f0e0f9773d07a5a0df2a",
+    "url": "/Our_Social_Media/static/js/1.f0e0f977.chunk.js"
+  },
+  {
+    "revision": "da676d9e20d6f1bac9e1",
+    "url": "/Our_Social_Media/static/js/runtime~main.da676d9e.js"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/Our_Social_Media/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/Our_Social_Media/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "3a2c8222eed87ef4acf5b34711fc3e07",
+    "url": "/Our_Social_Media/static/media/bg2.3a2c8222.png"
+  },
+  {
+    "revision": "44b3aa7bbe68f5f816d0a0c439d3a68c",
+    "url": "/Our_Social_Media/static/media/bg.44b3aa7b.jpg"
+  },
+  {
+    "revision": "be804bcf03afe794bd4e6006574edb60",
+    "url": "/Our_Social_Media/index.html"
+  }
+];
